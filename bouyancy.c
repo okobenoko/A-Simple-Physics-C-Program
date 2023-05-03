eb8ef3a810dda4f7eec2d68feb989f079dc3a8b7
okobenoko <okobenoko@outlook.com>
2023-05-03 12:26:33 +0800

    Commit: add base moudel of bouyancy
    There are some bugs and I will fix it soon

--- bouyancy.c
+++ bouyancy.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 void bouyancy_menu();
-
+void formula1(double * ptr_F);
 void bouyancy() {
 int number_formula;
+double F_Bouyancy;
 
 printf("Compute bouyancy\n");
 printf("Please select a formula\n");
 
 bouyancy_menu();
+
 scanf("%d", &number_formula);
 switch (number_formula) {
-    case 1: printf("Developing");
+    case 1: formula1(&F_Bouyancy); printf("%f", F_Bouyancy);
   }
 }
+
 void bouyancy_menu() {
-printf("(1) F=ρvg\n");
+printf("(1) F=ρVg\n");
+}
+
+void formula1(double * ptr_F) {
+double F, ρ, V, g = 9.8; 
+printf("Bouyancy compute: F=ρVg(g=9.8N/kg)");
+printf("please input the ρ:");
+scanf("%f", &ρ);
+printf("please input the V:");
+scanf("%f", &V);
+F = ρ * V * g;
+*ptr_F = F;
 }
